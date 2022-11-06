a755554e7735d5d60f400272765f0070bde4ac9e
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2022-11-07 01:52:45 +0300

    Расширяемая фабрика IoC

---
+++ src/IoC/IoC.cs
@@ -0,0 +1,36 @@
+﻿using SpaceBattleGame.Contracts.Commands;
+using System.Reflection;
+
+namespace IoC
+{
+    public static class IoC
+    {
+        private static ThreadLocal<ScopeController> _scopeController = new ThreadLocal<ScopeController>();
+
+        public static T Resolve<T>(string cmd, params object[] parameters)
+        {
+            if (_scopeController.Value == null)
+            {
+                _scopeController.Value = new ScopeController();
+            }
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type type = assembly.GetType(cmd);
+            if (type != null)
+            {
+                ConstructorInfo ctor = type.GetConstructors().FirstOrDefault();
+                T instance = (T)ctor.Invoke(new object[] { _scopeController.Value, parameters });                
+                return instance;
+            }
+            else
+            {
+                Dictionary<string, Func<object[], object>> dependencies = _scopeController.Value.GetDependenciesFromCurrentScope();                
+                if (dependencies.TryGetValue(cmd, out var value))
+                {
+                    return (T)(value(parameters));
+                }
+                return default(T);
+            }            
+        }
+    }
+}
